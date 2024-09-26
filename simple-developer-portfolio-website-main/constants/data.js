939f4d42731df504f0f19722469a9e1415f8b586
Salman Mustansar <139276188+saluop@users.noreply.github.com>
2024-09-27 00:55:56 +0500

    first commit

---
+++ simple-developer-portfolio-website-main/constants/data.js
@@ -0,0 +1,63 @@
+const userData = {
+  githubUsername: "saluop",
+  name: "Salman Mustansar",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/avatar.png",
+  email: "smustansar45@gmail.com",
+  phone: "+92 3264311502",
+  address: "Lahore, Pakistan.",
+  projects: [
+    {
+      title: "Llama-2 (An AI-chat bot)",
+      link: "",
+      imgUrl: "/llama2.jpg",
+    },
+    
+  ],
+  about: {
+    title:
+      "I'm a software engineer that loves building products and web applications that impact millions of lives",
+    description: [
+      `I've been developing full-stack application since I was 18 years old. I didn't know what full-stack meant at that time because the term was not coined back then. I'm so out of content right now that I'm literally crying writing this text since lorem text is so 2012 now.`,
+      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
+      `But now, I'm a legend. I've been coding in React, Angular, Vue, Svelte, Node.js, Express, Fastify, MongoDB, MySQL, Bootstrap, Tailwind, C, C++, C#, Rust, Haskell, Perl, PHP, Assembly, Logo and YouDontKnowShit programming language etc to name a few. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.`,
+    ],
+   
+  },
+  experience: [
+   
+    {
+      title: "Internship",
+      company: "Netsol",
+      year: "2024",
+      companyLink: "https://netsoltech.com/",
+      desc: "Worked with the team on a ride-hailing app and services.",
+    },
+    {
+      title: "Graduation",
+      company: "UMT, Lahore.",
+      year: "2026",
+      companyLink: "https://www.umt.edu.pk/",
+      desc: "Major in Software Engineering with CGPA of 3.2. Nobody asks this but it's okay.",
+    },
+    {
+      title: "High School",
+      company: "Beaconhouse, Lahore.",
+      year: "2020",
+      companyLink: "https://www.beaconhouse.net/",
+      desc: "Studied Biology as a major. LOL",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1lV-BLr_bmv8_IB6oRv2WVU7oi6HbW8P2/view?usp=drivesdk",
+  socialLinks: {
+    instagram: "https://www.instagram.com/_.salupriv/",
+    twitter: "",
+    linkedin: "https://www.linkedin.com/in/salman-mustansar-7345262ab?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BVEgbsnXzSN6S33kBbDB04A%3D%3D",
+    github: "https://github.com/saluop",
+    facebook: "https://facebook.com/",
+    peerlist: "",
+  },
+};
+
+export default userData;
